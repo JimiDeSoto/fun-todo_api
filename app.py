2e3ae3373cbc8f639cf9ba4b8320926662b727e2
root <root@JimiDeSoto.localdomain>
2019-09-12 11:12:42 +0200

    Dodane proste API

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 #!flask/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "PING ... ping ... PING"
+    return "API http://localhost:5000/todo/api/v1.0/tasks"
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Zakupy',
+        'description': u'Chleb, Mleko, Kefir', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Umyć samochód',
+        'description': u'Myjnia przy BP, żetony w schowku', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
