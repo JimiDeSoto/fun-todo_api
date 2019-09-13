d03ca8b175ebdfd715420e2656abcb4f5a2e36b3
root <root@JimiDeSoto.localdomain>
2019-09-13 12:41:59 +0200

    Auth 1.0

--- README.md
+++ README.md
@@ -3,10 +3,21 @@
 	apt-get install python3-venv
 	python3 -m venv flask
 	exitflask/bin/pip install flask
+	flask/bin/pip install flask-httpauth
+
+
 2. Użycie
+
+
 a) GET
 http://localhost:5000/todo/api/v1.0/tasks
 http://localhost:5000/todo/api/v1.0/tasks/"id"
+
+
 b) POST
 curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
+
+
+c) Update
+curl -i -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/todo/api/v1.0/tasks/2
 
--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'admin':
+        return 'password'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 @app.route('/')
@@ -24,10 +36,21 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 #GET
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-   return jsonify({'tasks': tasks})
+   return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+   #return jsonify({'tasks': tasks})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -49,8 +72,33 @@ def create_task():
     }
     tasks.append(task)
     return jsonify({'task': task}), 201
-
-
+	
+#Update Put
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+#Delete
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
 
 #error
 @app.errorhandler(404)
